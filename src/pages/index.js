560ddd0a766023b2bd57f821f1a8fd8e78a41cca
Wallace Pei <wpei78@gmail.com>
2023-06-06 11:58:51 -0400

    added new visual image features, responsive and static loaded images using gatsby plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 // Step 1: Import React
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I&rsquo;m making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Me and my young baby daughter Wren"
+        src="../images/Wren & Family-6.jpg"
+      />
     </Layout>
   )
 }
